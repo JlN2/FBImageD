a5fbca90627dc468409d1c710bcf3784b8eaba9d
JlN2 <jlnemuru@foxmail.com>
2016-09-04 00:26:18 +1000

    image pyramid (checked)

--- FBImagesD/main.cpp
+++ FBImagesD/main.cpp
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <opencv2\core\core.hpp>     // 有Mat数据类型
 #include <opencv2\highgui\highgui.hpp>
+#include <opencv2\imgproc\imgproc.hpp>
+#include <opencv2\opencv.hpp>
 
 using namespace std;
 using namespace cv;
@@ -12,9 +14,40 @@ const string imageFormat = ".jpg";
 const int FRAME_NUM = 10;
 const int REF = FRAME_NUM / 2;
 
+class Pyramid{
+public:
+
+	vector<Mat> pyramid;  // 默认是private
+
+
+	Pyramid(){}    
+	Pyramid(Mat & Image){     // 构造函数，对Image构造一个Image Pyramid
+		int cols = Image.cols ;
+		int rows = Image.rows;
+		int layerNum = 1;
+		while(cols > 400 || rows > 400){    // 计算这个Pyramid有几层，高斯金字塔最粗糙层image的长边不比400pixels大(文中说）
+			cols = cols >> 1;
+			rows = rows >> 1;
+			//printf("Col Num: %d, Row Num: %d\n", cols, rows);
+			layerNum++;
+		}
+		printf("Layer Num: %d\n", layerNum);
+		
+		// 计算Pyramid, 最高层是原图， pyramid[0]是最粗糙的
+		pyramid.resize(layerNum);
+		pyramid[layerNum - 1] = Image;
+		for(int i = layerNum - 2; i >= 0; i--){
+			Mat srcImage = pyramid[i+1];
+			// 用pyrDown进行下采样操作，执行了高斯金字塔建造的向下采样的步骤; 改变图像尺寸还可以用resize()
+			pyrDown(srcImage, pyramid[i], Size(srcImage.cols >> 1, srcImage.rows >> 1));
+		}
+	}
+};
+
 class FastBurstImagesDenoising{
 public:
 	vector<Mat> oriImageSet;      // 存储原来的每帧图片
+	vector<Pyramid*> imagePyramidSet;  // 图片金字塔（高斯金字塔）
 
 	void readBurstImages(const string fileDir){
 		Mat img;
@@ -27,15 +60,16 @@ public:
 	}
 
 	void showImages(vector<Mat> Images){
-		cout << "Image Size: " << Images.size() << endl;
+		int imageNum = Images.size();
+		cout << "Image Num: " << imageNum << endl;
+		cout << "Image Size(cols(x) * rows(y)): " << Images[0].size() << endl;  
 		char index[10];
-		for(int i = 0; i < FRAME_NUM; i++){
+		for(int i = 0; i < imageNum; i++){
 			sprintf_s(index, "%d", i);
 			imshow(index, Images[i]);
+			waitKey(1000);  // 等待1000 ms后窗口自动关闭  
 		}
-		waitKey(1000);
 	}
-
 
 };
 
@@ -43,7 +77,9 @@ FastBurstImagesDenoising FBID;
 
 int main(){
 	FBID.readBurstImages(fileDir);
-	FBID.showImages(FBID.oriImageSet);
+	//FBID.showImages(FBID.oriImageSet); 
+	Pyramid* pyramids = new Pyramid(FBID.oriImageSet[REF]);
+	FBID.showImages(pyramids->pyramid);
 
 	system("pause");
 
